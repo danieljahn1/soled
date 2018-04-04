6ef368f8741e64b65101eee46ea85070e8738fe6
e-masinas <emasinas@gmail.com>
2018-04-03 21:49:38 -0700

    Listings and auction updates

--- _frontend/src/redux/reducers.js
+++ _frontend/src/redux/reducers.js
@@ -1,11 +1,21 @@
 
 const initialState = {
-    listings: []
+    listings: [],
+    viewItems: [{}]
 }
 
 const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ("VIEW_ITEM"):
+            state = {
+                ...state,
+                viewItems: action.payload
+            }
+            break;
 
-
+    }
+    
+    return state;
 }
 
 export default rootReducer
